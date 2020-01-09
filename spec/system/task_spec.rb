2e973c5adfc15df1438f5ed4fe409b0acbed860c
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-01-09 20:13:04 +0900

    short_timeを使用した時刻のテストに修正しました。

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -56,6 +56,7 @@ RSpec.describe 'Task', type: :system do
 
   describe 'Task編集' do
     context '正常系' do
+      include ApplicationHelper
       it 'Taskを編集した場合、一覧画面で編集後の内容が表示されること' do
         # FIXME: テストが失敗するので修正してください
         project = create(:project)
@@ -64,7 +65,7 @@ RSpec.describe 'Task', type: :system do
         fill_in 'Deadline', with: Time.current
         click_button 'Update Task'
         click_link 'Back'
-        expect(find('.task_list')).to have_content(Time.current.strftime('%-m/%d %H:%M')) if task.deadline?
+        expect(find('.task_list')).to have_content(short_time(Time.current)) if task.deadline?
         expect(current_path).to eq project_tasks_path(project)
       end
 
