31afb9595c136bd158f5a853603094161d3370bf
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-01-09 18:57:59 +0900

    trait :doneに修正しました。

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -6,7 +6,7 @@ FactoryBot.define do
     to   = Date.parse("2019/12/31")
     deadline { Random.rand(from..to) }
     completion_date { nil }
-    trait :time_current_yesterday do
+    trait :done do
       completion_date { 1.day.ago }
     end
   end
